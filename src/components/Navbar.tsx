c145b4000e7db755846f1e432082e0fb3aa9893a
Sandesh258 <sandeshkumarsonigzr1807@gmail.com>
2025-03-11 12:19:11 +0530

    Icons Added in Sidebar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,10 @@
 import logo from '../assets/Logo.svg';
 
 const Navbar = () => (
-  <nav className="navbar navbar-light bg-light px-3 w-100 d-flex justify-content-between" style={{ width: "100vw" }}>
+  <nav className="navbar navbar-light bg-light px-3 w-100 d-flex justify-content-between navcustom">
     <img src={logo} alt="Company Logo" height="40" />
-    <button className="btn btn-outline-primary">Sign In</button>
+    <button type='submit'
+      className="btn btn-outline-primary">Sign In</button>
   </nav>
 );
 
